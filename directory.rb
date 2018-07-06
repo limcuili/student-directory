289b9c2d1d873981243bbfa6ca4120ed51a6e3d8
limcuili <limcuili@hotmail.com>
2018-07-06 10:07:36 +0100

    Refractor print statements

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+# Print the list of students in separate lines.
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -13,6 +15,4 @@ puts "Joffrey Baratheon"
 puts "Norman Bates"
 
 #print total number of students in one line.
-print "Overall, we have "
-print 11
-puts " great students"
+print "Overall, we have #{student_count} great students"
