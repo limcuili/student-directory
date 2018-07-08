0bc92a14ed635f1209116e7d9cdd26778e33975a
limcuili <limcuili@hotmail.com>
2018-07-09 00:02:59 +0100

    Add Excel save option

--- directory.rb
+++ directory.rb
@@ -17,9 +17,7 @@
 def interactive_menu
 	loop do
 		# print the menu and ask the user what to do
-		puts "1. Input the students"
-		puts "2. Show the students"
-		puts "9. Exit"
+		print_menu
 		# read the input and save it into a variable
 		selection = gets.chomp
 		# do what the user has asked
@@ -28,12 +26,33 @@ def interactive_menu
 				@students = input_student_name_cohort
 			when "2"
 				display
+			when "3"
+				save_students
 			when "9"
 				exit # this will cause the program to terminate
 			else
 				puts "I don't know what you meant, try again"
 		end
 	end
+end
+
+def save_students
+	# open the file for writing
+	file = File.open("students.csv", "w")
+	# iterate over the array of students
+	@students.each do |student|
+		student_data = [student[:name], student[:cohort]]
+		csv_line = student_data.join(",")
+		file.puts csv_line
+	end
+	file.close
+end
+
+def print_menu
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "3. Save the list to students.csv"
+	puts "9. Exit"
 end
 
 # Q6 Make everything that prints beauftifully aligned by using .center(n).
